7331532ce65f82a91aa904a140402038832696d6
Taro Sato <okomestudio@gmail.com>
2025-07-25 23:59:20 -0700

    Update init.el

--- init.el
+++ init.el
@@ -1,11 +1,26 @@
-(defconst ok-debug nil
-  "Global flag to activate all debug-related flag.")
-
-(setq debug-on-error ok-debug)
-
 ;; Configure use-package, init-straight.el or init-package.el:
-(load (expand-file-name "init.d/init-straight.el" user-emacs-directory))
+(load (expand-file-name "init.d/init-package.el" user-emacs-directory))
 
-(use-package magit)
+;;; Experimenting elfeed-tube installation
 
-(use-package org-roam)
+;; (use-package elfeed)
+;; (use-package elfeed-tube
+;;   :demand t
+;;   :config
+;;   (elfeed-tube-setup))
+
+;;; An example of minimal installation via vc
+
+;; (package-vc-install "https://github.com/okomestudio/pydoc-treesit.git"
+;; 		    nil nil 'pydoc-treesit)
+;; (add-to-list 'load-path
+;;              (expand-file-name "pydoc-treesit" package-user-dir))
+(use-package pydoc)
+(use-package pydoc-treesit
+  :vc (:url "https://github.com/okomestudio/pydoc-treesit.git")
+  :config
+  (add-to-list 'load-path
+	             (expand-file-name "pydoc-treesit/extensions"
+				                         package-user-dir)))
+(require 'pydoc-treesit)
+(require 'pydoc-names)
